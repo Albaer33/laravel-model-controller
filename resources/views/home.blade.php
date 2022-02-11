d61efd26e716c31a4d3e4a463b4ad0325e69cfba
Albaer33 <alisi3396@gmail.com>
2022-02-11 17:36:37 +0100

    added style to the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <h1>Homepage</h1>
+    <main>
+        <h1>Homepage</h1>
 
-    <h2>Movies list:</h2>
-
-    @foreach ($movies as $movie)
-        <h2>
-            <a href="{{ route('movie', ['id' => $movie->id]) }}">
-                {{ $movie->title }}
-            </a>
-        </h2>
-    @endforeach
+        <h2>Movies list:</h2>
+    
+        @foreach ($movies as $movie)
+            <h2 class="film-box">
+                <a href="{{ route('movie', ['id' => $movie->id]) }}">
+                    {{ $movie->title }}
+                </a>
+            </h2>
+        @endforeach
+    </main>
 </body>
 </html>
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -5,14 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <h1>Film details:</h1>
 
-    <div>Title: {{ $movie->title }}</div>
-    <div>Original Title: {{ $movie->original_title }}</div>
-    <div>Original Title: {{ $movie->nationality }}</div>
-    <div>Release date: {{ $movie->date }}</div>
-    <div>Vote: {{ $movie->vote }}</div>
+    <main class="details">
+        <h1>Film details:</h1>
+
+        <div>Title: {{ $movie->title }}</div>
+        <div>Original Title: {{ $movie->original_title }}</div>
+        <div>Original Title: {{ $movie->nationality }}</div>
+        <div>Release date: {{ $movie->date }}</div>
+        <div>Vote: {{ $movie->vote }}</div>
+    </main>
 </body>
 </html>
